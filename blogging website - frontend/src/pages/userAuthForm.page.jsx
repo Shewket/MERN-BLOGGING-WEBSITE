0295d7e246eadb89b6da577312020842912cff3c
Shewket Shewket <1842065771@qq.com>
2024-02-14 22:55:10 +0800

    Store User Sessions, User Navigation, Sign Out

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,14 +1,18 @@
 import InputBox from "../components/input.component";
 import uowIcon from "../imgs/logo-flowers.png"
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation"
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import {Toaster, toast} from 'react-hot-toast';
 import axios from "axios";
 import { storeInSession } from "../common/session";
+import { UserContext } from "../App";
 
 
-const UserAuthForm = ( {type} ) => {    
+const UserAuthForm = ( {type} ) => {  
+    
+    let {userAuth: {access_token}, setUserAuth} = useContext(UserContext)
+
 
 
     const userAuthThroughServer = (serverRoute, formData) => {
@@ -16,7 +20,7 @@ const UserAuthForm = ( {type} ) => {
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
         .then(({data}) => {
             storeInSession("user", JSON.stringify(data));
-            console.log(sessionStorage);
+            setUserAuth(data);
         })
         .catch(({response}) => {
             toast.error(response.data.error);
@@ -67,6 +71,9 @@ const UserAuthForm = ( {type} ) => {
     }
 
     return (
+        access_token ? 
+        <Navigate to="/" />
+        :
         <AnimationWrapper keyValue={type}>
             <section className="h-cover flex items-center justify-center">
                 <Toaster />
